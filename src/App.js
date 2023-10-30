f835740adcf26dc70e77354bd5ea8d5f15f70aed
Greta Va <greta.vasilevskyte@gmail.com>
2023-10-30 14:16:31 +0100

    Fix image selector

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import FormInput from './FormInput';
 // import { useState } from 'react';
 import Header from './Header';
 import Images from './Images';
-import SelectMeme from './SelectMeme';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <FormInput />
-      <SelectMeme />
       <Images />
     </div>
   );
--- src/FormInput.js
+++ src/FormInput.js
@@ -1,49 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Form() {
-  // inside parenthesesinclude setTopText, setBottomText, topText, bottomText
+export default function FormInput() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+
   return (
     <div className="input-field">
       <label htmlFor="topText">
         <input
           type="topText"
           placeholder="Enter your text"
-          // value={topText}
-          // onChange={(e) => setTopText(e.target.value)}
+          value={topText}
+          onChange={(e) => setTopText(e.target.value)}
         />
       </label>
       <label htmlFor="bottomText">
         <input
           type="bottomText"
           placeholder="Enter your text"
-          // value={bottomText}
-          //   onChange={(e) => setBottomText(e.target.value)}
+          value={bottomText}
+          onChange={(e) => setBottomText(e.target.value)}
         />
       </label>
       <br />
       <button className="generateBTN">Generate meme</button>
-      {/* <div className="meme-image">
-        <img
-          className="image"
-          src="https://api.memegen.link/images/doge.png"
-          alt="doge"
-        />
-      </div> */}
     </div>
   );
 }
-
-// For the select box (maybe separate component)
-
-// export default function FruitPicker() {
-//   return (
-//     <label>
-//       Pick a fruit:
-//       <select name="selectedFruit">
-//         <option value="apple">Apple</option>
-//         <option value="banana">Banana</option>
-//         <option value="orange">Orange</option>
-//       </select>
-//     </label>
-//   );
-// }
